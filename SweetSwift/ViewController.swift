6a3c084a8355ea57c3f96801ec80c222566f5c7d
suri0000 <yesle2005@naver.com>
2024-02-27 16:56:28 +0900

    first commit

---
+++ SweetSwift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SweetSwift
+//
+//  Created by 예슬 on 2/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var vendingMachine: UIImageView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
