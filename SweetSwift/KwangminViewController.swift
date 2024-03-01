d202e5edc5017ceaa020b0cba08db15abcad6b0b
김광민 <rlarhkdals@gimgwangmin-ui-MacBookAir.local>
2024-03-01 21:30:51 +0900

    kkmViewController
    .

--- SweetSwift/KwangminViewController.swift
+++ SweetSwift/KwangminViewController.swift
@@ -2,11 +2,10 @@ import UIKit
 
 class KwangminViewController: UIViewController {
 
-   
-   @IBOutlet weak var imageView: UIImageView!
    @IBOutlet weak var lblInfoShow1: UILabel!
    @IBOutlet weak var lblInfoShow2: UILabel!
-   
+   @IBOutlet weak var imageView: UIImageView!
+
    var info = ["name", "age", "mbti", "tmi", "blog"]
    var kkm = ["김광민", "26", "ENTJ", "백문이 불여일타 (100번 보는 것보다 1번 타이핑하는 것이 낫다!", "blog.naver.com/kex8977"]
    var count = 0
@@ -19,7 +18,7 @@ class KwangminViewController: UIViewController {
       //이미지 뷰에 그림파일 할당해주기
    }
    
-   @IBAction func btnChange(_ sender: Any){
+   @IBAction func btnChange(_ sender: UIButton) {
       if count == 5 {
          count = 0
       }
